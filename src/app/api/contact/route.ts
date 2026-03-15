b6e72e65aa1d97f501ce522513e78dbd2c90e1d8
Noah <151822733+AustrianNoah@users.noreply.github.com>
2026-03-15 20:07:19 +0100

    Update route.ts

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,13 +1,42 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-export async function POST(req:Request){
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 
-    const body = await req.json()
+export async function POST(req: Request) {
 
-    console.log("Contact Message:",body)
+    try {
 
-    return NextResponse.json({
-        success:true
-    })
+        const { name, email, message } = await req.json();
 
+        if(!DISCORD_WEBHOOK_URL) {
+            return NextResponse.json({ error: "Contact Form is not configured!"}, { status: 500 })
+        }
+
+        const payload = {
+            username: "Contact Form",
+            embeds: [
+                {
+                    title: `New Message from ${name}`,
+                    color: 3447003, // blue
+                    fields: [
+                        { name: "Name", value: name, inline: true },
+                        { name: "E-Mail", value: email, inline: true },
+                        { name: "Message", value: message, inline: false },
+                    ],
+                    timestamp: new Date().toISOString()
+                }
+            ]
+        }
+
+        await fetch(DISCORD_WEBHOOK_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload)
+        })
+
+        return NextResponse.json({ status: "success", message: "Contact has been received" }, { status: 200 })
+    } catch(error) {
+        console.error(error)
+        return NextResponse.json({ status: "error", error: error }, { status: 500 })
+    }
 }
